decef5b6ae959aa908314eaf784c88e5b5ff8641
tristanto <tristanto.kurniawan@gmail.com>
2016-05-05 10:39:07 +0700

    Add comment in program to explain why I do this

--- area_programs/calculate_area_and_circumference/src/main.rs
+++ area_programs/calculate_area_and_circumference/src/main.rs
@@ -1,18 +1,18 @@
 use std::io;
-use std::io::Write;
+use std::io::Write; // This is used for flushing
 
 fn main() {
     const PI: f64 = 3.14;
     let mut radius: String = String::new();
 
     print!("Enter radius of circle: ");
-    io::stdout().flush().unwrap();
+    io::stdout().flush().unwrap(); // macro print cannot autoflush
 
     io::stdin().read_line(&mut radius)
         .expect("Failed to read");
 
     let radius: f64 = radius.trim().parse()
-        .expect("Please input a number");
+        .expect("Please input a number"); // Don't forget except while parsing
 
     let area: f64 = PI * radius * radius;
     let circumference: f64 = PI * 2.0 * radius;
